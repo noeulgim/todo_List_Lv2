fd36bd191693c1c02941aceb6ebf47f4458a0421
noeulgim <noeul3240@gmail.com>
2024-07-03 16:32:49 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Router from './router/router';
+import Router from './router/Router';
 import { Provider } from 'react-redux';
 import store, { persistor } from './redux/config/store';
 import { PersistGate } from 'redux-persist/integration/react';

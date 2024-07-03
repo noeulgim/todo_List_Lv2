0411dc78fed3b6dfa88f9bad7bd3887fc090716a
noeulgim <noeul3240@gmail.com>
2024-07-03 16:31:59 +0900

    propstype update

--- src/components/todoList/listBox/Index.jsx
+++ src/components/todoList/listBox/Index.jsx
@@ -1,20 +1,19 @@
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
 import { StyledListBoxWrap, StyledListBoxWrapper, StyledDeleteButton, StyledDoneButton } from "./StyledListBox";
+import PropTypes from 'prop-types';
 
 const ListBox = ({list, ClickIsDoneHandler, ClickDeleteHandler, isDoneStatus, title}) => {
-    useEffect(() => {},[list]);
     return(
         <StyledListBoxWrapper>
         <h2>{title}</h2>
         {list.map((list)=> list.title && list.text && list.isDone === isDoneStatus && (
           <StyledListBoxWrap key={list.id}>
               <Link to={`/Detail/${list.id}`}>
-              <div className="listBox" >
+              <div>
                 <h3>{list.title}</h3>
                 <p>{list.text}</p>
               </div>
-              <div className="btnBox">
+              <div>
                 <StyledDeleteButton onClick={(e) => ClickDeleteHandler(e,list.id)}>삭제하기</StyledDeleteButton>
                 <StyledDoneButton onClick={(e) => ClickIsDoneHandler(e,list.id)}>{isDoneStatus ? '취소' : '완료'}</StyledDoneButton>
               </div>
@@ -24,5 +23,11 @@ const ListBox = ({list, ClickIsDoneHandler, ClickDeleteHandler, isDoneStatus, ti
       </StyledListBoxWrapper>
     );
 }
-
+ListBox.propTypes = {
+    list: PropTypes.array.isRequired,
+    ClickIsDoneHandler: PropTypes.func.isRequired, 
+    ClickDeleteHandler: PropTypes.func.isRequired,
+    isDoneStatus: PropTypes.bool.isRequired,
+    title: PropTypes.string.isRequired,
+}
 export default ListBox;
